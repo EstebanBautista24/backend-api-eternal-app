de91a83bc993a98775f82ff3621518de6a045529
Esteban <estebanbau36@gmail.com>
2024-12-11 18:15:52 -0500

    change database

--- src/main/java/com/eternal/api/Config/CorsConfig.java
+++ src/main/java/com/eternal/api/Config/CorsConfig.java
@@ -1,24 +1,17 @@
 package com.eternal.api.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://frontend-react-eternal-app.vercel.app"); // Dominio del frontend
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*"); // Permitir todas las cabeceras
-        config.setAllowCredentials(true); // Permitir credenciales si es necesario
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
