7a2252f353dfa035680c53b7434d10de667ad9e9
Esteban <estebanbau36@gmail.com>
2024-12-11 17:51:54 -0500

    change database

--- src/main/java/com/eternal/api/Config/CorsConfig.java
+++ src/main/java/com/eternal/api/Config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://frontend-react-eternal-app.vercel.app")
+                .allowedOrigins("frontend-react-eternal-app.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true);;
