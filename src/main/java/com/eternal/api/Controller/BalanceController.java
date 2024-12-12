378e7d5f3cee3f73c24a213caeda584f3a2c875f
Esteban <estebanbau36@gmail.com>
2024-12-11 19:16:57 -0500

    change cors policy

--- src/main/java/com/eternal/api/Controller/BalanceController.java
+++ src/main/java/com/eternal/api/Controller/BalanceController.java
@@ -5,11 +5,13 @@ import com.eternal.api.Service.BalanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://frontend-react-eternal-app.vercel.app")
 @RequestMapping("/balance")
 public class BalanceController {
 
