ecc0ef6dee91c732cec4bec7a759e88c653c6073
Esteban <estebanbau36@gmail.com>
2024-12-12 16:34:22 -0500

    change cors policy

---
+++ src/main/java/com/eternal/api/Config/CustomCorsFilter.java
@@ -0,0 +1,24 @@
+package com.eternal.api.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CustomCorsFilter {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://frontend-react-eternal-app.vercel.app"); // Cambia * por el dominio exacto
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
