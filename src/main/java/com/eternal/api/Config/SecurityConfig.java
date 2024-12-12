f011305936eda7b9ae8619582ad84b9b824c92b3
Esteban <estebanbau36@gmail.com>
2024-12-12 16:51:30 -0500

    change cors policy

--- src/main/java/com/eternal/api/Config/SecurityConfig.java
+++ src/main/java/com/eternal/api/Config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eternal.api.Config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -14,16 +15,30 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF (solo para APIs públicas o en pruebas)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()); // Permitir todas las solicitudes
+                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF (solo para pruebas)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .cors(Customizer.withDefaults()); // Asegúrate de habilitar CORS
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://frontend-react-eternal-app.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true); // Asegúrate de que esté activado si usas cookies
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
