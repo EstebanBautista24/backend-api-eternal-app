f6a978a7c51766393a841eae7e7429231e1c3f56
Esteban <estebanbau36@gmail.com>
2024-12-12 17:55:08 -0500

    change cors policy

--- src/main/java/com/eternal/api/Config/CorsConfig.java
+++ src/main/java/com/eternal/api/Config/CorsConfig.java
@@ -2,16 +2,17 @@ package com.eternal.api.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://frontend-react-eternal-2siagwd0m-estebanbautista24s-projects.vercel.app")
+                .allowedOrigins("https://frontend-react-eternal-2siagwd0m-estebanbautista24s-projects.vercel.app","*")
                 .allowedMethods("*")
-                .allowedHeaders("*","origin")
-                .allowCredentials(false);//
+                .allowedHeaders("*");
     }
 }
