55b391504ff4841f31fbea4942fbcf7a118a7d3b
Esteban <estebanbau36@gmail.com>
2024-12-12 16:45:51 -0500

    change cors policy

---
+++ src/main/java/com/eternal/api/Config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.eternal.api.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF (solo para APIs públicas o en pruebas)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()); // Permitir todas las solicitudes
+
+        return http.build();
+    }
+}
